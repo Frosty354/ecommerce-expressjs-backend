574c24029ed8889a38f85424c0bff285836ca9ac
Frosty354 <ayonchakraborty75@gmail.com>
2023-10-11 19:20:43 +0530

    mongodb key fix

--- src/connection/database.js
+++ src/connection/database.js
@@ -5,17 +5,12 @@ require('dotenv').config()
 // Create connections to different databases
 
 // Connect to the usercollection database
-const userCollectionConnection = mongoose.connect(`mongodb+srv://ayonchakraborty75:${process.env.MONGODB_URI_KEY}@cluster0.mfdl2yp.mongodb.net/usercollection?retryWrites=true&w=majority`, {
+const userCollectionConnection = mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 
-// Connect to the productdb database
-// const productDBConnection = mongoose.createConnection('mongodb+srv://ayonchakraborty75:eklENHCmUzZuI2jV@cluster0.mfdl2yp.mongodb.net/productdb?retryWrites=true&w=majority', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
 
 
 // Export the database connections
